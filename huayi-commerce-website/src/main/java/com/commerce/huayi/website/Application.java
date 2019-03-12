761a087166cbead68a4f133d95f3ba1e2e7560f1
xuyangyang <xuyangyang@rayvision.com>
2019-03-12 09:46:53 +0800

    增加swagger2文档工具

--- huayi-commerce-website/src/main/java/com/commerce/huayi/website/Application.java
+++ huayi-commerce-website/src/main/java/com/commerce/huayi/website/Application.java
@@ -11,6 +11,13 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.sql.DataSource;
 import javax.validation.Validation;
@@ -21,6 +28,7 @@ import javax.validation.ValidatorFactory;
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"com.commerce.huayi.website","com.commerce.huayi.cache"})
 @MapperScan(basePackages = "com.commerce.huayi.website.mapper")
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
@@ -43,5 +51,25 @@ public class Application {
         ValidatorFactory validatorFactory = Validation.byProvider(HibernateValidator.class).configure().failFast(true)
                 .buildValidatorFactory();
         return validatorFactory.getValidator();
+    }
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.commerce.huayi.website.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("huayi-commerce文档RESTful-APIs")
+                .description("")
+                .termsOfServiceUrl("https://github.com/yang7988/huayi-commerce.git")
+                .contact("上官小文")
+                .version("1.0")
+                .build();
     }
 }
--- huayi-commerce-website/src/main/java/com/commerce/huayi/website/controller/UserController.java
+++ huayi-commerce-website/src/main/java/com/commerce/huayi/website/controller/UserController.java
@@ -2,6 +2,8 @@ package com.commerce.huayi.website.controller;
 
 import com.commerce.huayi.api.ApiResponse;
 import com.commerce.huayi.website.entity.request.UserLoginReq;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,8 @@ import javax.validation.Valid;
 @RequestMapping(value = "/api/user")
 public class UserController {
 
+    @ApiOperation(value = "用户登录",notes = "根据用户名密码登陆")
+    @ApiImplicitParam(name = "userLoginReq", value = "用户登陆请求的实体", required = true, dataType = "UserLoginReq")
     @PostMapping(value = "/login")
     public ApiResponse login(@Valid UserLoginReq userLoginReq, BindingResult bindingResult) {
         return ApiResponse.returnSuccess();
