16984e526e12765f686a634f13cca57981fed318
xuyangyang <xuyangyang@rayvision.com>
2019-03-14 10:02:42 +0800

    管理员基础模型设计

---
+++ huayi-commerce-common/src/main/java/com/commerce/huayi/constant/Constant.java
@@ -0,0 +1,12 @@
+package com.commerce.huayi.constant;
+
+/**
+ * 常量
+ */
+public class Constant {
+    //已删除标识常量
+    public static final byte DELETED = 1;
+    //未删除标识常量
+    public static final byte NODELETE = 0;
+
+}
