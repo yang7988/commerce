4aa5b118b727f0a133ba83a0b721a3bfea5ed820
xuyangyang <xuyangyang@rayvision.com>
2019-03-15 18:45:45 +0800

    国际化

--- huayi-commerce-website/src/main/java/com/commerce/huayi/Interceptor/TranslateAspect.java
+++ huayi-commerce-website/src/main/java/com/commerce/huayi/Interceptor/TranslateAspect.java
@@ -5,19 +5,19 @@ import com.commerce.huayi.constant.LanguageEnum;
 import com.commerce.huayi.constant.TranslateDict;
 import org.apache.commons.collections.CollectionUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
