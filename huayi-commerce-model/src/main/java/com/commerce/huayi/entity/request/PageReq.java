5a0ddf9a1b179e469e6b5de435b928fcb0165661
xuyangyang <xuyangyang@rayvision.com>
2019-03-27 10:35:13 +0800

    添加分类、删除分类

--- huayi-commerce-model/src/main/java/com/commerce/huayi/entity/request/PageReq.java
+++ huayi-commerce-model/src/main/java/com/commerce/huayi/entity/request/PageReq.java
@@ -3,6 +3,8 @@ package com.commerce.huayi.entity.request;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * 数据分页信息
  * @date 2019-03-26
@@ -12,9 +14,11 @@ import io.swagger.annotations.ApiModelProperty;
 public class PageReq {
 
     @ApiModelProperty(value = "每页显示数据条数pageMaxSize",example = "10",dataType = "int")
+    @NotNull
     private int pageMaxSize;
 
     @ApiModelProperty(value = "当前页码pageIndex",example = "1",dataType = "int")
+    @NotNull
     private int pageIndex;
 
     public int getPageMaxSize() {
