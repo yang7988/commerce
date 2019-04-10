5b069ef3d272486972c816558cb0d677507741f5
xuyangyang <xuyangyang@rayvision.com>
2019-04-10 16:20:26 +0800

    分页查询重新封装

--- huayi-commerce-model/src/main/java/com/commerce/huayi/entity/request/PageCategoryGoodsRequest.java
+++ huayi-commerce-model/src/main/java/com/commerce/huayi/entity/request/PageCategoryGoodsRequest.java
@@ -1,12 +1,14 @@
 package com.commerce.huayi.entity.request;
 
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 import javax.validation.constraints.NotNull;
 
+@ApiModel(value = "获取分类的商品数据分页请求json对象")
 public class PageCategoryGoodsRequest extends PageRequest {
 
-    @ApiModelProperty(value = "数据主键id",example = "1")
+    @ApiModelProperty(value = "分类id",example = "1")
     @NotNull
     private Long id;
 
--- huayi-commerce-model/src/main/java/com/commerce/huayi/entity/request/UpdateCategoryReq.java
+++ huayi-commerce-model/src/main/java/com/commerce/huayi/entity/request/UpdateCategoryReq.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.NotNull;
 @ApiModel(value = "商品分类更新的请求json对象")
 public class UpdateCategoryReq extends CategoryReq {
 
-    @ApiModelProperty(value = "数据主键id",example = "1")
+    @ApiModelProperty(value = "数据主键分类id",example = "1")
     @NotNull
     private Long id;
 
