8a87bdbd0300b8d248e0e8bd325a6e410efaa389
shangguan <1058512559@qq.com>
2019-05-08 22:19:39 +0800

    管理员权限，留言板优化

--- huayi-commerce-website/src/main/java/com/commerce/huayi/controller/admin/CustomerMessageForAdminController.java
+++ huayi-commerce-website/src/main/java/com/commerce/huayi/controller/admin/CustomerMessageForAdminController.java
@@ -11,7 +11,6 @@ import com.commerce.huayi.utils.ExportExcel;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,8 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
